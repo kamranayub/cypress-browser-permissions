dc1361bc37fb3aff7797238f5864c11b6a49fe39
kamranayub <kamranayub@users.noreply.github.com>
2020-07-17 03:56:19 +0000

    deploy: c10090b6a2a4fe211f79c8b67e51f8312919318d

--- jest.setup.js
+++
@@ -1,9 +0,0 @@
-global.console = {
-    log: jest.fn(), // console.log are ignored in tests
-    info: jest.fn(),
-  
-    // Keep native behaviour for other methods, use those to print out things in your own tests, not `console.log`
-    error: console.error,
-    warn: console.warn,
-    debug: console.debug,
-  };
